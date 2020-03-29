78c8abcaef06d10a5542f4e79b925a5559bfd9c7
David Xiao <davidx@Huogens-MacBook-Pro.local>
2020-03-29 13:40:57 -0400

    input works

--- ace_editor.js
+++ ace_editor.js
@@ -1,4 +1,14 @@
+function parseRaw(data) {
+  new_data = "";
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] == '\n') {
+      new_data += " ";
+    } else {
+      new_data += data[i];
+    }
+  }
+  return new_data.split(" ");
+}
+
 let editor = ace.edit("editor");
 editor.getSession().setMode("ace/mode/plain_text");
-
-let raw_data = editor.session.getValue();
--- main.js
+++ main.js
@@ -14,143 +14,97 @@ const graph = {
   ]
 };
 
-let N = raw_data[0],
-    M = raw_data[2];
+let simulation, link, node;
 
-let newline = 0;
-let templink = {};
-for (let i = 4; i < editor.session.getLength() * 4 - 1; i++) {
-  if (raw_data[i] === " " || raw_data[i] === "\n") continue;
-  newline++;
-  console.log(i, raw_data[i]);
-  if (newline == 1) {
-    templink.source = parseInt(raw_data[i]);
-  } else {
-    templink.target = parseInt(raw_data[i]);
-    console.log(templink);
-    graph.links.push(templink);
-    templink = {};
-    newline = 0;
+function get_data() {
+  new_data = parseRaw(editor.session.getValue());
+  let N = parseInt(new_data[0]),
+      M = parseInt(new_data[1]);
+  let curr_ind = 2;
+  for (let i = 0; i < M; i++) {
+    let temp_link = {};
+    temp_link.source = parseInt(new_data[curr_ind]);
+    curr_ind++;
+    temp_link.target = parseInt(new_data[curr_ind]);
+    curr_ind++;
+
+    graph.links.push(temp_link);
   }
-}
-
-let tempnode = {};
-
-for (let i = 1; i <= N; i++) {
-  tempnode = {name: i};
-  graph.nodes.push(tempnode);
-}
-/*
-graph.nodes.push({name: "David"})
-console.log(graph.nodes[8]);
-*/
-
-// GRAPH SIMULATION
-let simulation = d3
-  .forceSimulation(graph.nodes)
-  .force(
-    "link",
-    d3
-      .forceLink()
-      .id(function(d) {
-        return d.name;
-      })
-      .distance(150)
-      .links(graph.links)
-  )
-  .force("charge", d3.forceManyBody().strength(-200))
-  .force("center", d3.forceCenter(WIDTH / 2, HEIGHT / 2))
-  .on("tick", ticked);
-
-// EDGE AND NODE BEHAVIOUR/PROPERTIES
-let link = SVG
-  .append("g")
-  .attr("class", "links")
-  .selectAll("line")
-  .data(graph.links)
-  .enter()
-  .append("line")
-  .attr("stroke-WIDTH", function(d) {
-    return 3;
-  });
-
-let node = SVG
-  .append("g")
-  .attr("class", "nodes")
-  .selectAll("circle")
-  .data(graph.nodes)
-  .enter().append("g")
-  .call(          // call here so we apply force to group 'g'. Otherwise a stutter will occur.
-    d3
-      .drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended)
-  );
-
-  // Attach circle to group
-  node.append("circle")
-  .attr("r", RADIUS)
-  .attr("fill", function(d) {
-    return "white";
-  });
-
-  // attach text to group
-  node.append("text")
-      .attr("x", 0)
-      .attr("dy", ".35em")
-      .attr("text-anchor", "middle")
-      .text(function(d) { return d.name; });
-
-function restart() {
-  graph.nodes = [];
-  graph.links = [];
-  let raw_data = editor.session.getValue();
-
-  let N = raw_data[0],
-      M = raw_data[2];
-
-  let newline = 0;
-  let templink = {};
-  for (let i = 4; i < editor.session.getLength() * 4 - 1; i++) {
-    if (raw_data[i] === " " || raw_data[i] === "\n") continue;
-    newline++;
-    console.log(i, raw_data[i]);
-    if (newline == 1) {
-      templink.source = parseInt(raw_data[i]);
-    } else {
-      templink.target = parseInt(raw_data[i]);
-      console.log(templink);
-      graph.links.push(templink);
-      templink = {};
-      newline = 0;
-    }
-  }
-
-  let tempnode = {};
 
   for (let i = 1; i <= N; i++) {
-    tempnode = {name: i};
-    graph.nodes.push(tempnode);
+    let temp_node = {name: i};
+    graph.nodes.push(temp_node);
   }
-  // Apply the general update pattern to the nodes.
-  node = node.data(graph.nodes);
-  node.exit().remove();
-  node = node.enter().append("circle").attr("fill", function(d) { return color(d.id); }).attr("r", 8).merge(node);
-
-  // Apply the general update pattern to the links.
-  link = link.data(graph.links);
-  link.exit().remove();
-  link = link.enter().append("line").merge(link);
-
-  // Update and restart the simulation.
-  simulation.nodes(graph.nodes);
-  simulation.force("link").links(graph.links);
-  simulation.alpha(1).restart();
 }
 
+function create_graph() {
+  SVG.selectAll("*").remove();
+  graph.nodes = [];
+  graph.links = [];
+  get_data();
+
+  // GRAPH SIMULATION
+  simulation = d3
+    .forceSimulation(graph.nodes)
+    .force(
+      "link",
+      d3
+        .forceLink()
+        .id(function(d) {
+          return d.name;
+        })
+        .distance(150)
+        .links(graph.links)
+    )
+    .force("charge", d3.forceManyBody().strength(-250))
+    .force("center", d3.forceCenter(WIDTH / 2, HEIGHT / 2))
+    .on("tick", ticked);
+
+  // EDGE AND NODE BEHAVIOUR/PROPERTIES
+  link = SVG
+    .append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter()
+    .append("line")
+    .attr("stroke-WIDTH", function(d) {
+      return 3;
+    });
+
+  node = SVG
+    .append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter().append("g")
+    .call(          // call here so we apply force to group 'g'. Otherwise a stutter will occur.
+      d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
+
+    // Attach circle to group
+    node.append("circle")
+    .attr("r", RADIUS)
+    .attr("fill", function(d) {
+      return "white";
+    });
+
+    // attach text to group
+    node.append("text")
+        .attr("x", 0)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .text(function(d) { return d.name; });
+}
+
+create_graph();
+
 document.getElementById("button").addEventListener("click", function() {
-  restart();
+  create_graph();
 });
 
 function ticked() {
