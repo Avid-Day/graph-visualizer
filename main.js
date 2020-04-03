ed73099b788caab5df612566b464cc969c447364
David Xiao <davidx@Huogens-MacBook-Pro.local>
2020-04-02 21:44:03 -0400

    implemented bfs in javascript

--- main.js
+++ main.js
@@ -141,3 +141,39 @@ function ticked() {
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
 }
+/*
+IMPLEMENT BFS
+// we give you edges = [{source: 1, target: 2}]
+// N = num nodes;
+let CURR_NODE = -1;
+function solve(edges, N) {
+    let vis = [], queue = [1], adj = [];
+
+    for (let i = 0; i <= N; i++) {
+        vis.push(0);
+        adj.push([]);
+    }
+    for (let i = 0; i < edges.length; i++) {
+        adj[edges[i].source.name].push(edges[i].target.name);
+    }
+
+    vis[1] = 1;
+
+    while (queue.length) {
+        let curr = queue[0];
+        queue.shift();
+        CURR_NODE = curr;
+        console.log(CURR_NODE);
+        console.log("TO:");
+        for (let i = 0; i < adj[curr].length; i++) {
+            let nxt = adj[curr][i];
+            if (vis[nxt]) continue;
+            console.log(nxt);
+            vis[nxt] = 1;
+            queue.push(nxt);
+        }
+    }
+}
+
+solve(graph.links, graph.nodes.length);
+*/
